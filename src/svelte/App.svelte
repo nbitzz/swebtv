8a8ea7e262f8a69449c3e26072b036c1a32b1fb2
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-05-18 20:01:05 +0000

    add

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import Sidebar from "./elm/Sidebar.svelte";
 
     onMount(() => {
         console.log("mounted")
@@ -13,36 +14,32 @@
         <!-- sidebar -->
         
         <div id="clgrad"></div>
-        <div class="sidebar" id="sidebar_main">
-
-            <div class="listItem">
-                <div class="icon">
-                    <img src="/assets/icons/tv.svg" alt="tv" />
-                </div>
-                <div class="content">
-                    <p>Movies</p>
-                </div>
-            </div>
-
-            <div class="listItem">
-                <div class="icon">
-                    <img src="/assets/icons/embed.svg" alt="embed" />
-                </div>
-                <div class="content">
-                    <p>Embeddables</p>
-                </div>
-            </div>
-
-            <div class="listItem">
-                <div class="icon">
-                    <img src="/assets/icons/settings.svg" alt="settings" />
-                </div>
-                <div class="content">
-                    <p>Settings</p>
-                </div>
-            </div>
-
-        </div>
+        <Sidebar width={250} level={2} items={[
+            {
+                id: "movies",
+                text: "Movies",
+                icon: {
+                    type: "image",
+                    content: "/assets/icons/tv.svg"
+                }
+            },
+            {
+                id: "embeddables",
+                text: "Embeddables",
+                icon: {
+                    type: "image",
+                    content: "/assets/icons/embeddables.svg"
+                }
+            },
+            {
+                id: "settings",
+                text: "Settings",
+                icon: {
+                    type: "image",
+                    content: "/assets/icons/settings.svg"
+                }
+            }
+        ]} />
         
     </div>
 
@@ -55,12 +52,6 @@
                     <br>simple and sweet. let's get started.
                 </span>
             </h1>
-        </div>
-
-        <div class="screen" id="screenMovies">
-            <div class="sidebar" id="movies_sidebar" style="--level: var(--sf1)">
-                
-            </div>
         </div>
 
     </div>
---
+++ src/svelte/elm/Sidebar.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+
+    interface SidebarItemIcon {
+        type: "image" | "text" | "html",
+        content: string
+    }
+
+    interface SidebarItem {
+        id: string,
+        text: string,
+        icon: SidebarItemIcon,
+    }
+
+    export let items:SidebarItem[] = [];
+
+    export let width: number = 200
+    export let level: number = 2
+
+</script>
+
+<div class="sidebar" id="sidebar_main" style:--wdth={width} style:--level={`var(--sf${level})`}>
+
+    {#each items as item (item.id)}
+
+    <div class="listItem">
+        <div class="icon">
+            {#if item.icon.type == "image"}
+                <img src={item.icon.content} alt={item.text} />
+            {:else if item.icon.type == "text"}
+                <p>{item.icon.content}</p>
+            {:else if item.icon.type == "html"}
+                {@html item.icon.content}
+            {/if}
+        </div>
+        <div class="content">
+            <p>{item.text}</p>
+        </div>
+    </div>
+
+    <!--
+    <div class="listItem">
+        <div class="icon">
+            <img src="/assets/icons/tv.svg" alt="tv" />
+        </div>
+        <div class="content">
+            <p>Movies</p>
+        </div>
+    </div>
+
+    <div class="listItem">
+        <div class="icon">
+            <img src="/assets/icons/embed.svg" alt="embed" />
+        </div>
+        <div class="content">
+            <p>Embeddables</p>
+        </div>
+    </div>
+
+    <div class="listItem">
+        <div class="icon">
+            <img src="/assets/icons/settings.svg" alt="settings" />
+        </div>
+        <div class="content">
+            <p>Settings</p>
+        </div>
+    </div>
+    -->
+
+    {/each}
+
+</div>
